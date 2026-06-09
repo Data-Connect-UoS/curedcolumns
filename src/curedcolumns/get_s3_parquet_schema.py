09fc8d1d53461be8a929c39e25b9a511492c7074
Ubuntu <ubuntu@ip-172-19-196-157.eu-west-2.compute.internal>
2026-06-09 15:44:13 +0000

    add private S3 VPC endpoints and directory paths in pyarrow

--- src/curedcolumns/get_s3_parquet_schema.py
+++ src/curedcolumns/get_s3_parquet_schema.py
@@ -21,13 +21,23 @@ def get_s3_parquet_schema(session, bucket: str, key: Union[str, Path]) -> pyarro
     # Connect to AWS S3
     # https://arrow.apache.org/docs/python/filesystems.html#s3
     credentials = session.get_credentials()
+
+    # Get the VPC Endpoint dynamically from boto3
+    s3_client = session.client("s3")
+    endpoint_url = s3_client.meta.endpoint_url
+
     s3_file_system = pyarrow.fs.S3FileSystem(
         access_key=credentials.access_key,
         secret_key=credentials.secret_key,
         region=session.region_name,
-        session_token=credentials.token
+        session_token=credentials.token,
+        endpoint_override=endpoint_url
     )
 
+    # Build dataset location
+    key_str = str(key)
+    if not key_str.endswith("/"):
+        key_str += "/"
     # Build data set location
     path = f"{bucket}/{key}"
 
